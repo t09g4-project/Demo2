085d5de767fb436c5d6b1b5f7aedba236af36a68
DarrenB8 <darrenbanh8@gmail.com>
2019-03-11 12:38:32 -0600

    Create SnakePosition.java

---
+++ SnakePosition.java
@@ -0,0 +1,17 @@
+
+public class SnakePosition {
+
+    public int col;
+    public int row;
+
+    SnakePosition(int row, int col) {
+        this.col = col;
+        this.row = row;
+    }
+
+    SnakePosition() {
+        col = 0;
+        row = 0;
+    }
+
+}
